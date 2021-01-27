0a857d99dd1ef6db5563951ae4029a2fb5db7d43
Nick Stein <sticknein@gmail.com>
2021-01-27 14:26:12 -0800

    New WFH videos

--- random-video.js
+++ random-video.js
@@ -1,19 +1,17 @@
 //WFH: Work From Home series, QS: Quarantine Sessions series//
-const WFH_8 = 'JUTPihcrSFw';
-const WFH_10 = 'j9F06lAOeGw';
-const WFH_14 = 'G672rCN5lKU';
+const WFH_17 = 'gR5N5j8xbH0';
+const WFH_18 = 'oPm_tKQEY_k';
+const WFH_21 = 'Ai-Vlfvbf14';
 const QS_1 = 'VQ15kYKsilk';
 const QS_2 = '1rY5x6U2oHo';
 const QS_3 = '7bolXtCzeHo';
 const humanimal = 'Dzpdgnv66Vk';
 const bedroom_set = 'COCrUY9XRYU'
 
-let videos = [WFH_8, WFH_10, WFH_14, QS_1, QS_2, QS_3, humanimal, bedroom_set]
+let videos = [WFH_17, WFH_18, WFH_21, QS_1, QS_2, QS_3, humanimal, bedroom_set]
 
-let button = document.getElementById('random-button')
+const button = document.getElementById('random-button')
 
 button.onclick = function() {
     document.getElementById('iframe').src = "https://www.youtube.com/embed/" + videos[Math.floor(Math.random() * videos.length)];
 }
-
-
