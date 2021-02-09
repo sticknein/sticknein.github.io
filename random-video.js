07252d66ad6df8d3b7f0d476154fab864e743cdf
Nick Stein <sticknein@gmail.com>
2021-02-09 09:49:31 -0800

    fixed random button

--- random-video.js
+++ random-video.js
@@ -1,29 +1,23 @@
 //WFH: Work From Home series, QS: Quarantine Sessions series//
-const WFH_17 = 'gR5N5j8xbH0';
-const WFH_18 = 'oPm_tKQEY_k';
-const WFH_21 = 'Ai-Vlfvbf14';
-const WFH_24 = 'fJNvU1kiDWA';
-const QS_1 = 'VQ15kYKsilk';
-const QS_2 = '1rY5x6U2oHo';
-const QS_3 = '7bolXtCzeHo';
+const wfh17 = 'gR5N5j8xbH0';
+const wfh18 = 'oPm_tKQEY_k';
+const wfh21 = 'Ai-Vlfvbf14';
+const wfh4 = 'fJNvU1kiDWA';
+const qs1 = 'VQ15kYKsilk';
+const qs2 = '1rY5x6U2oHo';
+const qs3 = '7bolXtCzeHo';
 const humanimal = 'Dzpdgnv66Vk';
 const bedroom_set = 'COCrUY9XRYU'
 
-let videos = [WFH_17, WFH_18, WFH_21, WFH_24, QS_1, QS_2, QS_3, bedroom_set]
-let playedVideos = [];
+let videos = [WFH_17, WFH_18, WFH_21, WFH_24, qs_1, QS_2, QS_3, bedroom_set]
 
 const button = document.getElementById('random-button')
 
 button.onclick = function() {
-    let random_video = "https://www.youtube.com/embed/" + videos[Math.floor(Math.random() * videos.length)];
-    document.getElementById('iframe').src = random_video;
-    videos.pop(random_video);
-    playedVideos.push(random_video)
-    if (videos.length <= 1) {
-        for (i=0; i < playedVideos.length; i++) {
-            playedVideos[i].pop();
-            videos.push(playedVideos[i]);
-        }
-    } 
+    let currentVideo = document.getElementById('iframe').src;
+    while (randomVideo !== currentVideo) {
+        let randomVideo = "https://www.youtube.com/embed/" + videos[Math.floor(Math.random() * videos.length)];
+    document.getElementById('iframe').src = randomVideo;
+    }   
 }
 
